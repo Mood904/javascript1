3523c059690f8a207e74bb1d6a9196e3d2f58404
Karren Kilpatrick <mood7swangs@gmail.com>
2021-09-15 12:19:28 -0400

    Lesson1cmplete

--- Lesson 1/script.js
+++ Lesson 1/script.js
@@ -1,3 +1,8 @@
 let username=prompt ("Hey you! Yeah you.  What is your name?")
  
 alert ( username +","+ " "+ "Cherish the imperfections")
+let z=prompt  ("What is the temparature in your location(in F)")
+let y=(z-32) *5 /9
+alert (z + " "+ "is"+" " +y+" "+"degrees Celsius")
+let W=(z * 1.609344)
+alert (z+" "+ "in miles is also" + " "+W+" "+"in KM" )
